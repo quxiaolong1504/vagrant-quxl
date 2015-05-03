7ab2c560e3a82d349ab3f349feaabfb79c130a70
quxiaolong1504 <quxl@snbway.com>
2015-05-03 21:11:36 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,3 +2,9 @@ sudo apt-get update
 sudo apt-get install git
 sudo apt-get install sudo apt-get install mysql-server
 wget https://bootstrap.pypa.io/ez_setup.py -O- | sudo python
+sudo pip install virtualenv
+sudo pip install virtualenvwrappe
+mkdir $HOME/.virtualenvs
+echo export WORKON_HOME=$HOME/.virtualenvs>>~/.bashrc
+echo source /usr/local/bin/virtualenvwrapper.sh>>~/.bashrc
+source ~/.bashrc
